c2d0b522a8d491d583eb981c6aae94c5574db33f
Tiago Sabadini <sabadini.tiago@gmail.com>
2024-04-01 17:28:11 -0300

    first commit

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+function comprar() {
+  // Aqui você pode adicionar código para redirecionar o usuário para o processo de compra
+  alert("Você será redirecionado para o processo de compra. Obrigado por adquirir nosso eBook!");
+  window.location.href = "./ebook.html";
+}
